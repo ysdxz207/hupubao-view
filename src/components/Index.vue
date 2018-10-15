40e9517a5cc8727ba70250a8c5c2eacb77205726
Moses <ysdxz207@qq.com>
2018-10-15 15:30:30 +0800

    canvas test

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-    <el-container style="height: 100vh;">
+    <el-container style="height: 100vh;position: relative;z-index: 0">
         <el-header>
             <menu-top></menu-top>
         </el-header>
@@ -19,6 +19,7 @@
                 <a href="https://github.com/ysdxz207/hupubao-view" target="_blank">Version&nbsp;2.0</a>
             </el-footer>
         </el-main>
+        <canvas id="canvas_background" style="display:block"></canvas>
     </el-container>
 
 </template>
@@ -34,9 +35,39 @@
             return {
             }
         },
+        mounted() {
+            this.setBackgroundCanvas()
+        },
         methods: {
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
+            },
+            setBackgroundCanvas() {
+                let canvas = document.querySelector("#canvas_background")
+                let ctx = canvas.getContext("2d")
+
+                canvas.width = document.documentElement.clientWidth
+                canvas.height = document.documentElement.clientHeight
+
+                ctx.moveTo(80, 80)
+                ctx.lineTo(240, 80)
+                ctx.stroke()
+
+
+                function random(min, max) {
+                    if (arguments.length < 2) {
+                        max = min
+                        min = 0
+                    }
+
+                    if (min > max) {
+                        let hold = max
+                        max = min
+                        min = hold
+                    }
+
+                    return Math.floor(Math.random() * (max - min + 1)) + min
+                }
             }
         },
         created() {
@@ -75,5 +106,9 @@
         margin-top: 80px;
     }
 
+    #canvas_background {
+        position: absolute;
+        z-index: 0;
+    }
 
 </style>
